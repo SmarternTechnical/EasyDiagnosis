087be75760c75d2902339728be8f7d7377a883ca
SmarternTechnical <164067549+SmarternTechnical@users.noreply.github.com>
2024-11-04 23:33:29 +0530

    Update urls.py

--- Backend/easy_diagnosis/app/urls.py
+++ Backend/easy_diagnosis/app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+from .views import SignUpView, LoginView
 
 urlpatterns = [
     # path('', views.home, name='home'),
     path('update-table/', views.upload_csv_and_replace_table, name='upload_csv_and_replace_table'),
     path('get-category-details/', views.get_category_details, name='get_category_details'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),  
 
 ]
 
