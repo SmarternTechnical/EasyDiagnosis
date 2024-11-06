389717ae9f2b594862e3f38f085329dd128b4694
SmarternTechnical <164067549+SmarternTechnical@users.noreply.github.com>
2024-11-06 23:46:39 +0530

    Add files via upload

--- Backend/easy_diagnosis/app/admin.py
+++ Backend/easy_diagnosis/app/admin.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
-from .models import MedicalServiceCategory, PharmaSupport, UserAccount
+from .models import MedicalServiceCategory, PharmaSupport, UserAccount, Doctors
 
 # Register your models here.
 class MedicalServiceCategoryAdmin(admin.ModelAdmin):
     list_display = [field.name for field in MedicalServiceCategory._meta.fields]
 
 class PharmaSupportAdmin(admin.ModelAdmin):
-    # Display all fields as columns in the list view
     list_display = [field.name for field in PharmaSupport._meta.fields]
 
 class UserAccountAdmin(admin.ModelAdmin):
-    # Display all fields as columns in the list view
     list_display = [field.name for field in UserAccount._meta.fields]
+
+class DoctorsAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Doctors._meta.fields]
 
 admin.site.register(MedicalServiceCategory, MedicalServiceCategoryAdmin)
 admin.site.register(PharmaSupport, PharmaSupportAdmin)
 admin.site.register(UserAccount, UserAccountAdmin)
+admin.site.register(Doctors, DoctorsAdmin)
--- Backend/easy_diagnosis/app/models.py
+++ Backend/easy_diagnosis/app/models.py
@@ -30,3 +30,20 @@ class UserAccount(models.Model):
 
     def __str__(self):
         return self.email
+
+class Doctors(models.Model):
+    d_name = models.CharField(max_length=255, default='Default Name')
+    category = models.CharField(max_length=100)
+    d_image = models.CharField(max_length=255, default=None)
+    expertise = models.CharField(max_length=255, default=None) 
+    qualification = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    clinic = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    languages = models.CharField(max_length=255)
+    work_experience = models.CharField(max_length=255)
+    d_about = models.TextField() 
+    rating = models.FloatField(default=0.0) 
+
+    def __str__(self):
+        return self.d_name
