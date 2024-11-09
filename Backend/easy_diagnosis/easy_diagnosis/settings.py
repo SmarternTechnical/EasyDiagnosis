b6176dfc9a736906db24f86446ae74be57460919
aryan138 <apathania206@gmail.com>
2024-11-09 13:05:33 +0530

    CORS Added

--- Backend/easy_diagnosis/easy_diagnosis/settings.py
+++ Backend/easy_diagnosis/easy_diagnosis/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'corsheaders',
     'rest_framework',
 ]
 
@@ -49,6 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Add your React app URL here
 ]
 
 ROOT_URLCONF = 'easy_diagnosis.urls'
